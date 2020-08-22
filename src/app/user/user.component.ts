887cbeaebc7fc55fbf2e546fb3f69c527dac6994
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-22 23:12:07 +0900

    Getting Closer to the Core of Observables

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,6 +12,17 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) {
   }
 
+/**
+ * Q. Why don't we unsubscribe in the user comonent, here where we also set up a subscription? (valid question)
+ * A. The answer simply is Anuglar does that for you.
+ * For the observables provided by Angular, like params but also any other observable you'll encounter
+ * throughout this course that is provided by an Angular package or by a feature provided by Angaulr,
+ * all these Angular observables are managed by Angaulr.
+ * 
+ * Therefore, we don't need to unsubscribe manually here.
+ * Angular will take care about this.
+ */
+
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
